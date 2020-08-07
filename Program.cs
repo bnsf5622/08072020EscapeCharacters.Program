f465b870f15a1a6a0cadca2cee7d243142ef04f2
chuck francis <chuck_francis10@yahoo.com>
2020-08-07 13:06:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08072020EscapeCharacters.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("The cow says,\" Moo\"");
+            //Console.WriteLine("1\t2\t3\t");
+            //Console.WriteLine("1\n2\\n3");
+            //Console.WriteLine("c:\\Data\\MyFile.txt");
+            Console.WriteLine(@"one
+two
+three");
+            Console.ReadLine();
+
+        }
+    }
+}
